663462d49059161d944d7ce0b59a9c1c1a8b924a
MikeyDN <Mikeydn24@gmail.com>
2022-09-10 12:59:59 +0300

    Working contract solidity 0.8.9

--- contract/lottery.sol
+++ contract/lottery.sol
@@ -1,10 +1,38 @@
-^pragma solidity ^0.8.9
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
 
 contract Lottery {
-  array public participants;
-  string public manager;
-  constructor() {
+    address public manager;
+    address[] public participants;
 
-  }
-  fucntion enter()
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        participants.push(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function pickWinner() public onlyOwner {
+        uint winner = random() % participants.length;
+        payable(participants[winner]).transfer(address(this).balance);
+        participants = new address[](0);
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return participants;
+    }
+
+    function random() private view returns (uint) { //unit = uint256
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants)));
+    }
+
 }
