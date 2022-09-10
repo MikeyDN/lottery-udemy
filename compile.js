c04e29db8def1b545cdc4ee842ece99c31b2cb54
MikeyDN <Mikeydn24@gmail.com>
2022-09-10 13:10:14 +0300

    Compatible with solidity 0.8.9

---
+++ compile.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const contractPath = path.resolve(__dirname, 'contract', 'lottery.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+
+const input = {
+	language: 'Solidity',
+	sources: { 'lottery.sol': { content: source, } },
+	settings: {
+		outputSelection: { '*': { '*': ['*'] } }
+	}
+};
+module.exports = JSON.parse(
+	solc.compile(JSON.stringify(input))
+).contracts['lottery.sol'].Lottery;
